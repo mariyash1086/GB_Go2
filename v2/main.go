3d29737859c938e5039d313005fce0611611c762
Mariyash1086 <milly10031986@gmail.com>
2022-02-22 16:41:17 +0400

    its a release v2.1.1

---
+++ v2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var m int
+	m = 10
+	fmt.Println(m)
+}
+
+//new func for pre-release
+func createNewFunc() {
+
+}
+
+//release 2.1.1
+func createNewRelease() {
+
+}
